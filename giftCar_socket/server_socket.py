8e85c9b6917c92e7d398c8cfdad2f3a2c972a202
Wo-Ki <1874421129@qq.com>
2018-01-06 20:44:05 +0800

    update 4

--- giftCar_socket/server_socket.py
+++ giftCar_socket/server_socket.py
@@ -32,51 +32,41 @@ def handle_client(client_socket, client_address):
             request_data = client_socket.recv(1024)
             if request_data:
                 print "request_data:", request_data
-                if request_data.find("GET /video") != -1:
-                    # 浏览器视频
-                    print "Brower Linked"
-                    while True:
-                        responseStartLine = "HTTP/1.1 200 OK\r\n"
-                        responseHeader = "Server: my server\r\n"
-                        responseBody = gen(Camera())
-                        response = responseStartLine + responseHeader + "\r\n" + responseBody
-                        client_socket.send(bytes(response))
-                else:
-                    try:
-                        json_datas = request_data.split(",")
-                        for json_data in json_datas:
-                            json_data = json.loads(json_data)
-                            if json_data.get("UD") is not None:
-                                car_pwm_ctrl.left_wheel(int(json_data.get("UD")))
-                                car_pwm_ctrl.right_wheel(int(json_data.get("UD")))
-                            if json_data.get("LR") is not None:
-                                car_pwm_ctrl.dir_ctrl(int(json_data.get("LR")))
-                            if json_data.get("YH") is not None:
-                                car_pwm_ctrl.yh_ctrl(int(json_data.get("YH")))
-                            if json_data.get("YV") is not None:
-                                car_pwm_ctrl.yv_ctrl(int(json_data.get("YV")))
-                            if json_data.get("AU") is not None:
-                                robotArmCtrl.upCtrl(int(json_data.get("AU")))
-                            if json_data.get("AD") is not None:
-                                robotArmCtrl.downCtrl(int(json_data.get("AD")))
-                            if json_data.get("AL") is not None:
-                                robotArmCtrl.leftCtrl(int(json_data.get("AL")))
-                            if json_data.get("AR") is not None:
-                                robotArmCtrl.rightCtrl(int(json_data.get("AR")))
-                            if json_data.get("AC") is not None:
-                                robotArmCtrl.clean()
-                    except:
-                        pass
+                try:
+                    json_datas = request_data.split(",")
+                    for json_data in json_datas:
+                        json_data = json.loads(json_data)
+                        if json_data.get("UD") is not None:
+                            car_pwm_ctrl.left_wheel(int(json_data.get("UD")))
+                            car_pwm_ctrl.right_wheel(int(json_data.get("UD")))
+                        if json_data.get("LR") is not None:
+                            car_pwm_ctrl.dir_ctrl(int(json_data.get("LR")))
+                        if json_data.get("YH") is not None:
+                            car_pwm_ctrl.yh_ctrl(int(json_data.get("YH")))
+                        if json_data.get("YV") is not None:
+                            car_pwm_ctrl.yv_ctrl(int(json_data.get("YV")))
+                        if json_data.get("AU") is not None:
+                            robotArmCtrl.upCtrl(int(json_data.get("AU")))
+                        if json_data.get("AD") is not None:
+                            robotArmCtrl.downCtrl(int(json_data.get("AD")))
+                        if json_data.get("AL") is not None:
+                            robotArmCtrl.leftCtrl(int(json_data.get("AL")))
+                        if json_data.get("AR") is not None:
+                            robotArmCtrl.rightCtrl(int(json_data.get("AR")))
+                        if json_data.get("AC") is not None:
+                            robotArmCtrl.clean()
+                except:
+                    pass
             else:
                 print "[%s, %s] : disconnect" % client_address
                 client_socket.close()
                 return
-        except Exception, e:
-            print e
-            print "[%s, %s] : disconnect" % client_address
-            client_socket.close()
-            return
 
+        except Exception, e:
+        print e
+        print "[%s, %s] : disconnect" % client_address
+        client_socket.close()
+        return
 
 if __name__ == "__main__":
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
