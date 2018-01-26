c83227befb13209288918cc355ab7ef49f1049f7
Wo-Ki <1874421129@qq.com>
2018-01-26 16:23:22 +0800

    update 4

--- giftCar_socket/testFiles/speed_test.py
+++ giftCar_socket/testFiles/speed_test.py
@@ -16,32 +16,11 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(wheelRAPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(wheelRBPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-wheelRACount = 0
-wheelRBCount = 0
 wheelRCount = 0
-
 
 wheelRADir = True
 wheelRBDir = True
 
-
-def wheelRAFunc(channel):  # 边缘检测回调函数，详情在参见链接中
-    global wheelRACount, wheelRADir  # 设置为全局变量
-    if GPIO.event_detected(wheelRAPin):  # 检测到一个脉冲则脉冲数加1
-        wheelRACount = wheelRACount + 1
-        if GPIO.input(wheelRBPin) == GPIO.HIGH:
-            wheelRADir = True
-        else:
-            wheelRADir = False
-    # print "counter RA:", wheelRACount
-
-
-def wheelRBFunc(channel):  # 这里的channel和channel1无须赋确定值，但笔者测试过，不能不写
-    global wheelRBCount
-    if GPIO.event_detected(wheelRBPin):
-        wheelRBCount = wheelRBCount + 1
-
-    # print "counter RB:", wheelRBCount
 
 def countFunc(channel):
     global wheelRCount
@@ -50,10 +29,18 @@ def countFunc(channel):
     print "count:", wheelRCount
 
 
-# GPIO.add_event_detect(wheelRBPin, GPIO.RISING, callback=wheelRBFunc)  # 在引脚上添加上升临界值检测再回调
-# GPIO.add_event_detect(wheelRAPin, GPIO.RISING, callback=wheelRAFunc)
+def dirFunc(channel):
+    global wheelRADir
+    if GPIO.event_detected(wheelRAPin):
+        if GPIO.input(wheelRBPin) == GPIO.HIGH:
+            wheelRADir = True
+        else:
+            wheelRADir = False
+
+
 GPIO.add_event_detect(wheelRBPin, GPIO.BOTH, callback=countFunc)  # 在引脚上添加上升临界值检测再回调
 GPIO.add_event_detect(wheelRAPin, GPIO.BOTH, callback=countFunc)
+GPIO.add_event_detect(wheelRAPin, GPIO.RISING, callback=dirFunc)
 print "****test begin****"
 
 lastTime = 0.0
@@ -61,10 +48,9 @@ lastTime = 0.0
 while True:
     if lastTime == 0 or time.time() - lastTime >= 0.5:
         lastTime = time.time()
-        radiusSpeed = (wheelRACount / 150 * 2 * math.pi) * 2
+        radiusSpeed = (wheelRCount / 520 * 2 * math.pi) * 2
         if not wheelRADir and radiusSpeed != 0:
             radiusSpeed = -1 * radiusSpeed
         # print "speed radius A: %.2f rad/s" % radiusSpeed
         # print "speed A: %.2f m/s" % (radiusSpeed * 0.03)
         wheelRACount = 0
-
