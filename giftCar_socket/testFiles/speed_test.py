5c3d5e4e85b462450dbd2f9188a7127184828c48
Wo-Ki <1874421129@qq.com>
2018-01-26 15:05:12 +0800

    update 4

--- giftCar_socket/testFiles/speed_test.py
+++ giftCar_socket/testFiles/speed_test.py
@@ -4,8 +4,10 @@
 # creator = wangkai
 # creation time = 2018/1/14 16:01
 
+
+from __future__ import division
 import RPi.GPIO as GPIO
-import time, sys
+import time, sys, math
 
 wheelRAPin = 35
 wheelRBPin = 18
@@ -21,14 +23,14 @@ def wheelRAFunc(channel):  # 边缘检测回调函数，详情在参见链接中
     global wheelRACount  # 设置为全局变量
     if GPIO.event_detected(wheelRAPin):  # 检测到一个脉冲则脉冲数加1
         wheelRACount = wheelRACount + 1
-    print "counter RA:", wheelRACount
+    # print "counter RA:", wheelRACount
 
 
 def wheelRBFunc(channel):  # 这里的channel和channel1无须赋确定值，但笔者测试过，不能不写
     global wheelRBCount
     if GPIO.event_detected(wheelRBPin):
         wheelRBCount = wheelRBCount + 1
-    print "counter RB:", wheelRBCount
+    # print "counter RB:", wheelRBCount
 
 
 GPIO.add_event_detect(wheelRBPin, GPIO.RISING, callback=wheelRBFunc)  # 在引脚上添加上升临界值检测再回调
@@ -36,5 +38,11 @@ GPIO.add_event_detect(wheelRAPin, GPIO.RISING, callback=wheelRAFunc)
 
 print "****test begin****"
 
+lastTime = 0.0
+
 while True:
-    pass
+    if lastTime == 0 or time.time() - lastTime >= 1:
+        lastTime = time.time()
+        print "speed radius A:", wheelRACount / 150 * 2 * math.pi, " rad/s"
+        print "speed A:", wheelRACount / 150 * 2 * math.pi * 0.029, "m/s"
+        wheelRACount = 0
