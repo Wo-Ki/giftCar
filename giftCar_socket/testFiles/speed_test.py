f7fbab03ce4740dc38d49d3d2fd48291bf1602a6
Wo-Ki <1874421129@qq.com>
2018-01-26 14:32:51 +0800

    update 4

--- giftCar_socket/testFiles/speed_test.py
+++ giftCar_socket/testFiles/speed_test.py
@@ -7,29 +7,32 @@
 import RPi.GPIO as GPIO
 import time, sys
 
-GPIO.setmode(gpio.BOARD)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # 通过18号引脚读取左轮脉冲数据
-GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # 通过35号引脚读取右轮脉冲数据
-counter = 0  # 左轮脉冲初值
-counter1 = 0  # 右轮脉冲初值
+wheelRAPin = 35
+wheelRBPin = 18
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(wheelRBPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(wheelRAPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+wheelRACount = 0
+wheelRBCount = 0
 
 
-def my_callback(channel):  # 边缘检测回调函数，详情在参见链接中
-    global counter  # 设置为全局变量
-    if GPIO.event_detected(18):  # 检测到一个脉冲则脉冲数加1
-        counter = counter + 1
-    print "counter:", counter
+def wheelRAFunc(channel):  # 边缘检测回调函数，详情在参见链接中
+    global wheelRACount  # 设置为全局变量
+    if GPIO.event_detected(wheelRAPin):  # 检测到一个脉冲则脉冲数加1
+        counter = wheelRAPin + 1
+    print "counter RA:", counter
 
 
-def my_callback1(channel1):  # 这里的channel和channel1无须赋确定值，但笔者测试过，不能不写
-    global counter1
-    if GPIO.event_detected(35):
-        counter1 = counter1 + 1
-    print "counter1:", counter1
+def wheelRBFunc(channel):  # 这里的channel和channel1无须赋确定值，但笔者测试过，不能不写
+    global wheelRBCount
+    if GPIO.event_detected(wheelRBPin):
+        counter = wheelRBCount + 1
+    print "counter RB:", counter
 
 
-GPIO.add_event_detect(18, GPIO.RISING, callback=my_callback)  # 在引脚上添加上升临界值检测再回调
-GPIO.add_event_detect(35, GPIO.RISING, callback=my_callback1)
+GPIO.add_event_detect(wheelRBPin, GPIO.RISING, callback=wheelRBFunc)  # 在引脚上添加上升临界值检测再回调
+GPIO.add_event_detect(wheelRAPin, GPIO.RISING, callback=wheelRAFunc)
 
 print "****test begin****"
 
