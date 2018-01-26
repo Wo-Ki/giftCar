c649b0725809995343d0572bb4d77267e8a6934d
Wo-Ki <1874421129@qq.com>
2018-01-26 14:37:10 +0800

    update 4

--- giftCar_socket/testFiles/speed_test.py
+++ giftCar_socket/testFiles/speed_test.py
@@ -20,15 +20,15 @@ wheelRBCount = 0
 def wheelRAFunc(channel):  # 边缘检测回调函数，详情在参见链接中
     global wheelRACount  # 设置为全局变量
     if GPIO.event_detected(wheelRAPin):  # 检测到一个脉冲则脉冲数加1
-        counter = wheelRAPin + 1
-    print "counter RA:", counter
+        wheelRACount = wheelRACount + 1
+    print "counter RA:", wheelRACount
 
 
 def wheelRBFunc(channel):  # 这里的channel和channel1无须赋确定值，但笔者测试过，不能不写
     global wheelRBCount
     if GPIO.event_detected(wheelRBPin):
-        counter = wheelRBCount + 1
-    print "counter RB:", counter
+        wheelRBCount = wheelRBCount + 1
+    print "counter RB:", wheelRBCount
 
 
 GPIO.add_event_detect(wheelRBPin, GPIO.RISING, callback=wheelRBFunc)  # 在引脚上添加上升临界值检测再回调
