91d38b030ccc4311be66b4e612beb6a0baa69f87
Wo-Ki <1874421129@qq.com>
2018-03-13 17:10:50 +0800

    update 4

--- giftCar_socket/testFiles/speed_test.py
+++ giftCar_socket/testFiles/speed_test.py
@@ -8,6 +8,7 @@
 from __future__ import division
 import RPi.GPIO as GPIO
 import time, sys, math
+import threading
 
 wheelRAPin = 35
 wheelRBPin = 18
@@ -26,21 +27,29 @@ def countFunc(channel):
     global wheelRCount
     if GPIO.event_detected(wheelRAPin) or GPIO.event_detected(wheelRBPin):
         wheelRCount += 1
-    print "count:", wheelRCount
+        # print "count:", wheelRCount
 
 
 GPIO.add_event_detect(wheelRBPin, GPIO.BOTH, callback=countFunc)  # 在引脚上添加上升临界值检测再回调
 GPIO.add_event_detect(wheelRAPin, GPIO.BOTH, callback=countFunc)
 print "****test begin****"
 
+
+def speed():
+    global wheelRCount
+    radiusSpeed = (wheelRCount / 520.0 * (2 * math.pi)) * 2
+    if not wheelRADir and radiusSpeed != 0:
+        radiusSpeed = -1 * radiusSpeed
+    print "speed radius A: %.2f rad/s" % radiusSpeed
+    print "speed A: %.2f m/s" % (radiusSpeed * 0.03)
+    wheelRCount = 0
+
+
+timer = threading.Timer(0.5, speed)
+
 lastTime = 0.0
 
 while True:
     if lastTime == 0 or time.time() - lastTime >= 0.5:
         lastTime = time.time()
-        radiusSpeed = (wheelRCount / 520.0 * (2 * math.pi)) * 2
-        if not wheelRADir and radiusSpeed != 0:
-            radiusSpeed = -1 * radiusSpeed
-        # print "speed radius A: %.2f rad/s" % radiusSpeed
-        # print "speed A: %.2f m/s" % (radiusSpeed * 0.03)
-        # wheelRCount = 0
+        print "out"
